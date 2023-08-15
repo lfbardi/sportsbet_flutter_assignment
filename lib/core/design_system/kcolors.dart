014ffe96370835b74ffc67795464669a57b6898a
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-08-15 09:13:40 -0300

    custom colors

---
+++ lib/core/design_system/kcolors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const kBackgroundColor = Color(0xFFE5E5E5);
+const kPrimaryColor = Color(0xFF6200EE);
+const kPrimaryColorDark = Color(0xFF3700B3);
+const kPrimaryColorLight = Color(0xFFBB86FC);
